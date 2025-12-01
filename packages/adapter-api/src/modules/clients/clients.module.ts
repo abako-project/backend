7e1608acd4f27718e5aef2f4ebd2c76f26ab6184
b-avb <brayan.vargas.dev@gmail.com>
2025-12-01 12:04:57 -0500

    test: inject connection for mongoose-sequence

--- packages/adapter-api/src/modules/clients/clients.module.ts
+++ packages/adapter-api/src/modules/clients/clients.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
-import { Client, ClientSchema } from '../../database/schemas/client.schema';
+import { Client, ClientSchema, ClientSchemaFactory } from '../../database/schemas/client.schema';
 import { Project, ProjectSchema } from '../../database/schemas/project.schema';
+import { Connection } from 'mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Client.name,
+        inject: [getConnectionToken()],
+        useFactory: (connection: Connection) => {
+          return ClientSchemaFactory(connection);
+        },
+      },
+    ]),
     MongooseModule.forFeature([
       { name: Client.name, schema: ClientSchema },
       { name: Project.name, schema: ProjectSchema },
